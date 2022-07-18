9a9b35746c8c2e6673d7291e5c15be239612f36a
Omer Munk <omerm@moveo.co.il>
2022-07-18 22:42:56 +0300

    utils version1

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #include <stdio.h>
+#include "util_functions/utils_omer1.c"
 
 int main() {
     printf("Hello, World!\n");
+
+    int n = 215;
+    int arr[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    dec_to_bin(n, arr);
+    print_bin(arr);
+    printf("\n");
+    bin_to_dec(10, arr);
+    printf("\n");
+//    printf("%c\n", 33);
+//    printf("%c\n", 64);
+//    printf("%c\n", 35);
+//    printf("%c\n", 36);
+//    printf("%c\n", 37);
+//    printf("%c\n", 94);
+    printf("%c\n", 38);
+    printf("%c\n", 42);
+    printf("%c\n", 60);
+    printf("%c\n", 62);
+//    printf("%c\n", 97);
+    char arr2[10] = {'!', '!', '!', '!', '!', '!', '!', '!', '!', '!'};
+    char arr3[10] = {'v', '!', '!', '!', '!', '!', '!', '!', '!', '!'};
+    dec_to_base32(64, arr2);
+    print_base32(arr2);
+    printf("\n");
+    base32_to_dec(10, arr3);
+
+//    printf("%d", '!');
+
+
     return 0;
 }
---
+++ util_functions/utils_omer1.c
@@ -0,0 +1,132 @@
+//
+// Created by Omer Munk on 18/07/2022.
+//
+#include <math.h>
+
+
+//function to convert decimal to binary
+void dec_to_bin(int n, int *arr) {
+    int i = 0;
+    while (n > 0) {
+        arr[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+}
+
+void print_bin(int *arr) {
+    int i;
+    for (i = 9; i > -1; i--) {
+        printf("%d", arr[i]);
+    }
+}
+
+void bin_to_dec(int length, int *arr) {
+    int i = 0;
+    int sum = 0;
+    while (i < length) {
+        sum = sum + arr[i] * pow(2, i);
+        i++;
+    }
+    printf("%d", sum);
+}
+
+void dec_to_base32(int base32num, char *arr) {
+    int quotient, remainder, i;
+    i = 0;
+    quotient = base32num;
+    while (quotient != 0) {
+        remainder = quotient % 32;
+        switch (remainder) {
+            case 0:
+                arr[i++] = '!'; //33
+                break;
+            case 1:
+                arr[i++] = '@'; //64
+                break;
+            case 2:
+                arr[i++] = '#'; //35
+                break;
+            case 3:
+                arr[i++] = '$'; //36
+                break;
+            case 4:
+                arr[i++] = '%'; //37
+                break;
+            case 5:
+                arr[i++] = '^'; //94
+                break;
+            case 6:
+                arr[i++] = '&'; //38
+                break;
+            case 7:
+                arr[i++] = '*'; //42
+                break;
+            case 8:
+                arr[i++] = '<'; //60
+                break;
+            case 9:
+                arr[i++] = '>'; //62
+                break;
+            default:
+                arr[i++] = (char)(remainder + 87);
+                break;
+        }
+        quotient = quotient / 32;
+    }
+}
+
+
+void base32_to_dec(int length, char *arr) {
+    int i = 0;
+    int sum = 0;
+    while (i < length) {
+        switch (arr[i]) {
+            case '!':
+                sum = sum + 0 * pow(32, i);
+                break;
+            case '@':
+                sum = sum + 1 * pow(32, i);
+                break;
+            case '#':
+                sum = sum + 2 * pow(32, i);
+                break;
+            case '$':
+                sum = sum + 3 * pow(32, i);
+                break;
+            case '%':
+                sum = sum + 4 * pow(32, i);
+                break;
+            case '^':
+                sum = sum + 5 * pow(32, i);
+                break;
+            case '&':
+                sum = sum + 6 * pow(32, i);
+                break;
+            case '*':
+                sum = sum + 7 * pow(32, i);
+                break;
+            case '<':
+                sum = sum + 8 * pow(32, i);
+                break;
+            case '>':
+                sum = sum + 9 * pow(32, i);
+                break;
+            default:
+                sum = sum + (arr[i] - 87) * pow(32, i);
+                break;
+        }
+        i++;
+    }
+    printf("%d", sum);
+}
+
+void print_base32(char *arr) {
+    int i;
+    for (i = 9; i > -1; i--) {
+        printf("%c", arr[i]);
+    }
+}
+
+
+
